33bf9185d8ef5f5b50e2702e1543a72a65383691
Adam Kalt <adam.kalt.1@gmail.com>
2018-04-12 04:12:10 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,7 +1,9 @@
 def oxford_comma(array)
-array.join
-array.join(" and ")
-if array.length > 2
+if array.length == 1
+  array.join
+elsif array.length == 2
+  array.join(" and ")
+elsif array.length > 2
   #array.insert(1, ',')
   #array.insert(2, ', and')
 #elsif array.length > 3
