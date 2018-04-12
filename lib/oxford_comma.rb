1155bb29aeb05bc89f1520a4a1fdb9c9a0d4083a
Adam Kalt <adam.kalt.1@gmail.com>
2018-04-12 03:57:07 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,7 +2,8 @@ def oxford_comma(array)
 array.join
 array.join(" and ")
 if array.length == 3
-  array.join("and")
+  array.insert(1, ',')
+  array.insert(2, ', and')
 else array.length > 3
   array.sort
 end
